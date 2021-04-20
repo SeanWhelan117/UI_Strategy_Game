94e8cb1ce539e1d631133da106119ec6a816fb0c
(Student) - Sean Whelan <c00250016@itcarlow.ie>
2021-04-20 12:39:31 +0100

    Added register, manifest and service worker

---
+++ service_worker.js
@@ -0,0 +1,26 @@
+var cacheName = 'SeanWhelan117-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/push.css',
+  '/js/newScript.js',
+  'controller.html'
+];
+
+/* Cache contents when Offline See Cache */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline, examine Cache Storage */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
